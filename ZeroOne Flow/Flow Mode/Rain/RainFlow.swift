1de81401fec53b0a5a0ec2b15da313ce1eb7c80f
frank-n-01 <95156426+frank-n-01@users.noreply.github.com>
2022-01-27 22:20:40 +0900

    Rain flow mode random range modified

--- ZeroOne Flow/Flow Mode/Rain/RainFlow.swift
+++ ZeroOne Flow/Flow Mode/Rain/RainFlow.swift
@@ -20,13 +20,9 @@ struct RainFlow: View {
         }
         .onReceive(Timer.publish(every: rain.isFlowing ? rain.interval : 100,
                                  on: .current, in: .common).autoconnect()) { _ in
-            // Send notification to the rain drops.
-            NotificationCenter.default.post(
-                name: Notification.Name("rain"), object: nil
-            )
-            // Appear one by one.
-            if count < loop {
-                count += 1
+            count += 1
+            if count > 100000 {
+                count = 0
             }
         }
     }
@@ -34,7 +30,7 @@ struct RainFlow: View {
     private var rainDrops: some View {
         ForEach(0 ..< loop, id: \.self) { i in
             if i < count {
-                RainDrop(rain: rain, height: $height, width: $width)
+                RainDrop(rain: rain, height: $height, width: $width, count: $count)
             }
         }
     }
@@ -58,6 +54,7 @@ struct RainDrop: View {
     @ObservedObject var rain: RainViewModel
     @Binding var height: CGFloat
     @Binding var width: CGFloat
+    @Binding var count: Int
     @State private var content = ""
     @State private var fontSize: CGFloat = 0
     @State private var design: Font.Design = .monospaced
@@ -78,21 +75,9 @@ struct RainDrop: View {
             .fixedSize(horizontal: false, vertical: true)
             .background(contentSizeGetter)
             .onAppear {
-                // Initialize the parameters.
-                fontSize = rain.fonts.sizeRange.getRandomSizeInRange()
-                design = rain.fonts.design.value
-                weight = rain.fonts.weight.value
-                
-                position.x = CGFloat.random(in: 0...width)
-                position.y = CGFloat.random(in: 0...height)
-                
-                for _ in 0 ..< Int.random(in: 1...Int(rain.length)) {
-                    content += ContentMaker.make(with: rain.contents) + "\n"
-                }
+                initializeParameters()
             }
-            .onReceive(NotificationCenter.default.publisher(
-                for: Notification.Name("rain"))
-            ) { _ in
+            .onChange(of: count) { _ in
                 move()
             }
     }
@@ -109,6 +94,19 @@ struct RainDrop: View {
                     top = -(height / 2)
                     bottom = height + geometry.size.height
                 }
+        }
+    }
+    
+    private func initializeParameters() {
+        fontSize = rain.fonts.sizeRange.getRandomSizeInRange()
+        design = rain.fonts.design.value
+        weight = rain.fonts.weight.value
+        
+        position.x = CGFloat.random(in: 0...width)
+        position.y = CGFloat.random(in: 0...height)
+        
+        for _ in 0 ..< Int.random(in: 1...Int(rain.length)) {
+            content += ContentMaker.make(with: rain.contents) + "\n"
         }
     }
     
--- ZeroOne Flow/Flow Mode/Rain/RainViewModel.swift
+++ ZeroOne Flow/Flow Mode/Rain/RainViewModel.swift
@@ -51,11 +51,11 @@ class RainViewModel: FlowModeViewModel {
     override func makeRandomStyle() {
         super.makeRandomStyle()
         
-        scale = Double.random(in: 1...200)
-        interval = Double.random(in: 0.01...0.05)
+        scale = Double.random(in: 1...150)
+        interval = Double.random(in: 0.01...0.2)
         fonts.sizeRange.random(max: 100)
-        length = Double.random(in: 50...300)
-        step = CGFloat.random(in: 100...500)
+        length = Double.random(in: 50...200)
+        step = CGFloat.random(in: 50...200)
     }
     
     override func applyUserDefaults() {
